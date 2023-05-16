815f9cf9c354fc3ca94ff3eb92fe3ebbaed9097c
Przemek <przemyslaw_baczek@o2.pl>
2023-05-16 23:09:04 +0200

    commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const express = require('express');
+const app=express();
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Oczekuje na porcie ${port}...`))
+
+app.use(express.json());
+
+const ejs=require('ejs');
+const path= require("path");
+
+app.set('view engine', 'ejs');  //konieczne doinstalowanie modułu
+
+app.set('views', './pages');
+
+const expressSession = require('express-session');
+app.use(express.static(path.join(__dirname, 'style')));
+app.get('/', (req, res) => {
+    res.render('index', {uzytkownik: 'testowy'});
+});
