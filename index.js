f5aef0c1e82219c7c8f47c6df9fd6c83ec47cd37
Przemek <przemyslaw_baczek@o2.pl>
2023-05-30 19:01:54 +0200

    commit

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ app.set('view engine', 'ejs');  //konieczne doinstalowanie modułu
 app.set('views', './pages');
 
 const expressSession = require('express-session');
-app.use(express.static(path.join(__dirname, 'style')));
+app.use(express.static(path.join(__dirname, 'pages')));
 app.get('/', (req, res) => {
-    res.render('index', {uzytkownik: 'testowy'});
+    res.render('views/index', {images: 'plakat'});
 });
+
+app.get('/login', (req, res) => {
+    res.redirect('/sesja');
+});
+
+app.get('/sesja', (req, res) => {
+    res.render('views/loggedin', {images: 'plakat'});
+});
+
+app.get('/reset', (req, res) => {
+    res.render('views/reset', {images: 'plakat'});
+});
+
+app.get('/rejestracja', (req, res) => {
+    res.render('views/utworz_konto', {images: 'plakat'});
+});
+
---
+++ pages/scripts/skrypt.js
@@ -0,0 +1,41 @@
+
+function logowanie(){
+
+    var user = '{{request.user}}';
+    const element_loguj=document.getElementById('loguj');
+    if(element_loguj){
+        element_loguj.addEventListener('click', function(event) {
+            let login=document.getElementById('wsp_login').value;
+            let haslo=document.getElementById('wsp_haslo').value;
+            const http = new XMLHttpRequest()
+            http.open("GET", "http://localhost:3000/login");
+            http.send();
+            window.location="http://localhost:3000/sesja";
+            http.onload = () => console.log(http.responseText);
+        });
+    }
+
+    const element_wyloguj=document.getElementById('wyloguj');
+    if(element_wyloguj){
+        element_wyloguj.addEventListener('click', function(event){
+            const http = new XMLHttpRequest()
+            http.open("GET", "http://localhost:3000/");
+            http.send();
+            window.location="http://localhost:3000/";
+            http.onload = () => console.log(http.responseText);
+        });
+    }
+
+    const element_rejestracja=document.getElementById('rejestruj');
+    if(element_rejestracja){
+        element_rejestracja.addEventListener('click', function(event){
+            const http = new XMLHttpRequest()
+            http.open("GET", "http://localhost:3000/");
+            http.send();
+            window.location="http://localhost:3000/rejestracja";
+            http.onload = () => console.log(http.responseText);
+        });
+    }
+
+
+}
